375e23ee6c5cb42e47d4ad137d9acc33f9647cd8
Rogério M A Júnior <135529116+rgjuni0r@users.noreply.github.com>
2023-06-17 15:23:06 -0300

    Add files via upload
    MODELO DE CONEXÃO VIA API COM CHATGPT
    Este repositório contém um exemplo de código em Java que demonstra como estabelecer uma conexão com a API do ChatGPT e obter uma resposta gerada pelo modelo GPT-3.5-turbo.
    
    Descrição do Código
    O código é uma implementação simples que utiliza bibliotecas nativas do Java para realizar a conexão HTTP com a API do ChatGPT. Ele permite enviar uma solicitação para a API, receber a resposta e extrair o conteúdo desejado.
    
    O programa inicia importando as bibliotecas necessárias para lidar com a leitura de dados de entrada, saída de dados, conexão HTTP e manipulação de URLs.
    
    Em seguida, o código define duas constantes: apiUrl, que contém o endereço da API do ChatGPT, e apiKey, que representa a chave de API necessária para autenticação.
    
    Dentro do método main, o código cria uma instância de URL utilizando o valor de apiUrl. Em seguida, abre uma conexão HTTP com a URL por meio de um objeto HttpURLConnection. A solicitação é configurada com o método POST, o cabeçalho Content-Type é definido como "application/json; utf-8" e a autorização é configurada utilizando a chave de API.
    
    Os parâmetros da solicitação são armazenados em variáveis, como o modelo do GPT, a entrada do usuário e o número máximo de tokens. O corpo da solicitação é montado em formato JSON, contendo esses parâmetros.
    
    A solicitação é enviada para o servidor, por meio do objeto OutputStream da conexão HTTP. Em seguida, é obtido o código de resposta HTTP da conexão e impresso na saída do console.
    
    O código verifica se o código de resposta é diferente de 200, o que indica um erro na solicitação. Se for o caso, são exibidas mensagens de erro adequadas com base no código de resposta. Caso contrário, a resposta do servidor é lida e armazenada em um objeto StringBuilder.
    
    A resposta é então manipulada para extrair o conteúdo desejado, utilizando índices para identificar a posição inicial e final do trecho de texto desejado no JSON. O conteúdo extraído é impresso na saída do console.
    
    Uso
    Este código pode ser utilizado como ponto de partida para desenvolver aplicações que interagem com a API do ChatGPT. É importante lembrar que é necessário ter uma chave de API válida para autenticação.
    
    Autor
    O código foi desenvolvido por Rogério Moreira Alves Júnior em 17/06/2023 (BR-PT).
    
    Título
    MODELO DE CONEXÃO VIA API COM CHATGPT
    
    Modelo
    O código utiliza o modelo GPT-3.5-turbo do ChatGPT para gerar as respostas.

---
+++ Exemplo.java
@@ -0,0 +1,141 @@
+/**
+ * @author ROGÉRIO MOREIRA ALVES JÚNIOR
+ * @date 17/06/2023 (BR-PT)
+ * @title MODELO DE CONEXÃO VIA API COM CHATGPT
+ * @model "gpt-3.5-turbo"
+ */
+
+package chatgpt;
+
+//IMPORTANDO AS BIBLIOTECAS NECESSÁRIAS
+import java.io.BufferedReader; // Biblioteca para leitura de dados de entrada
+import java.io.InputStreamReader; // Biblioteca para leitura de dados de entrada
+import java.io.OutputStream; // Biblioteca para saída de dados
+import java.net.HttpURLConnection; // Biblioteca para conexão HTTP
+import java.net.URL; // Biblioteca para trabalhar com URLs
+
+public class Exemplo {
+	// DEFININDO AS CONSTANTES DE URL E API KEY
+	private static final String apiUrl = "https://api.openai.com/v1/chat/completions";
+	private static final String apiKey = "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";
+
+	public static void main(String[] args) throws Exception {
+		// CRIANDO CONSTRUTOR: CRIANDO URL COM O ENDEREÇO DA API
+		URL url = new URL(apiUrl); // O construtor da classe URL é responsável por criar uma instância de URL a
+									// partir de uma string que representa a URL desejada. O valor da variável
+									// apiUrl é passado como argumento para o construtor, especificando a URL que
+									// será utilizada no objeto url.
+
+		// ## ABRINDO CONEXÃO HTTP COM A URL
+		HttpURLConnection request = (HttpURLConnection) url.openConnection();
+
+		// CONFIGURAÇÃO DA SOLICITAÇÃO
+		request.setRequestMethod("POST"); // Definindo o método da solicitação como POST para envio de dados ao servidor
+		request.setRequestProperty("Content-Type", "application/json; utf-8"); // Definindo as propriedades da
+																				// solicitação, como o tipo de conteúdo
+																				// e a autorização (O tipo de conteúdo é
+																				// definido como "application/json;
+																				// utf-8" para indicar que o corpo da
+																				// solicitação está em formato JSON)
+
+		request.setRequestProperty("Authorization", "Bearer " + apiKey); // A autorização é definida como "Bearer"
+																			// seguido da chave de API para autenticar a
+																			// solicitação
+
+		request.setDoOutput(true); // Habilitando a saída de dados na conexão para enviar o corpo da solicitação ao
+									// servidor
+
+		// ARMAZENANDO OS PARÂMETROS EM VARIÁVEIS
+		String model = "gpt-3.5-turbo"; // Armazenando o modelo do GPT
+		String userInput = "Olá, Chat GPT!"; // Armazenando a entrada do usuário
+		int maxTokens = 50; // Definindo o número máximo de tokens
+
+		// MONTANDO O CORPO DA SOLICITAÇÃO NO FORMATO JSON
+		String requestData = String.format(
+				"{\"model\": \"%s\", \"messages\": [{\"role\": \"user\", \"content\": \"%s\"}], \"max_tokens\": %d}",
+				model, userInput, maxTokens);
+
+		// ENVIANDO A SOLICITAÇÃO PARA O SERVIDOR
+		try (OutputStream os = request.getOutputStream()) {
+			byte[] input = requestData.getBytes("utf-8");
+			os.write(input, 0, input.length);
+		}
+
+		// OBTENDO O CÓDIGO DE RESPOSTA HTTP DA CONEXÃO
+		int responseCode = request.getResponseCode();
+		System.out.println("Response Code: " + responseCode); // Imprime o código de resposta HTTP
+
+		// VERIFICANDO SE O CÓDIGO DE RESPOSTA É DIFERENTE DE 200
+		if (responseCode != 200) {
+			if (responseCode == 400) {
+				System.out.println("Erro de conexão: 400 Bad Request");
+				System.out.println(
+						"Indica que minha solicitação possui uma sintaxe inválida ou não pode ser processada pelo servidor.");
+
+			} else if (responseCode == 401) {
+				System.out.println("Erro de conexão: 401 Unauthorized");
+				System.out.println(
+						" Indica que eu não tenho permissão para acessar o recurso solicitado. Preciso fornecer autenticação válida.");
+
+			} else if (responseCode == 403) {
+				System.out.println("Erro de conexão: 403 Forbidden");
+				System.out.println(
+						"Indica que não tenho permissão para acessar o recurso solicitado, mesmo que eu esteja autenticado. Isso ocorre devido a restrições de acesso impostas pelo servidor.");
+
+			} else if (responseCode == 404) {
+				System.out.println("Erro de conexão: 404 Not Found");
+				System.out.println(
+						" Indica que o recurso solicitado não foi encontrado no servidor. Pode ser devido a um URL incorreto, recurso movido ou excluído.");
+
+			} else if (responseCode == 500) {
+				System.out.println("Erro de conexão: 500 Internal Server Error");
+				System.out.println(
+						"Indica que ocorreu um erro interno no servidor durante o processamento da minha solicitação. É um problema no código do servidor.");
+
+			} else {
+				System.out.println("Erro de conexão: " + responseCode);
+			}
+
+			return; // SE A RESPOSTA FOR DIFERENTE DE 200, ENCERRA A OPERAÇÃO
+		}
+
+		// LENDO A RESPOSTA DO SERVIDOR
+		StringBuilder response = new StringBuilder();
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream(), "utf-8"))) {
+			String responseLine = null;
+			while ((responseLine = br.readLine()) != null) {
+				response.append(responseLine.trim());
+			}
+		}
+
+		// EXTRAINDO A RESPOSTA DESEJADA DO JSON DE RESPOSTA (MANIPULANDO RESPOSTA JSON
+		// - EXTRAINDO O CORPO DE TEXTO RECEBIDO)
+		String responseString = response.toString(); // Aqui, convertemos o objeto response do tipo StringBuilder para
+														// uma string.
+		int startIndex = responseString.indexOf("\"content\": \""); // Procuramos a posição inicial do trecho de texto
+																	// que indica o início da resposta desejada no JSON.
+																	// Essa posição é armazenada na variável startIndex.
+
+		if (startIndex != -1) { // Verificamos se o startIndex é diferente de -1, o que significa que
+								// encontramos a resposta desejada. Caso contrário, a resposta não foi
+								// encontrada.
+			startIndex += "\"content\": \"".length(); // Se a resposta foi encontrada, ajustamos o startIndex
+														// adicionando o comprimento do trecho \"content\": \", para
+														// obter a posição inicial do conteúdo da resposta.
+			int endIndex = responseString.indexOf("\"", startIndex); // Procuramos a posição final do conteúdo da
+																		// resposta, que é o próximo caractere de aspas
+																		// duplas após o startIndex.
+			String extractedResponse = responseString.substring(startIndex, endIndex); // Usando o método substring,
+																						// extraímos o conteúdo da
+																						// resposta com base nas
+																						// posições startIndex e
+																						// endIndex.
+
+			// Imprimindo a resposta extraída (Imprimimos a resposta extraída na saída do
+			// console, adicionando uma mensagem informativa.)
+			System.out.println("Resposta GPT: " + extractedResponse);
+		} else {
+			System.out.println("Resposta não encontrada.");
+		}
+	}
+}
